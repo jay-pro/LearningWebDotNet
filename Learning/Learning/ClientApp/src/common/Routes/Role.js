c643972408fd6a277b38518eece1a31df54741e9
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-09-22 12:55:45 +0700

    upload routers and services for roles

---
+++ Learning/Learning/ClientApp/src/common/Routes/Role.js
@@ -0,0 +1,8 @@
+export const Role = {
+    SystemAdmin: "SystemAdmin",
+    Teacher: "Teacher",
+    Student: "Student",
+    Mentor: "Mentor",
+    Instructor: "Instructor",
+    ClassAdmin: "ClassAdmin"
+}
